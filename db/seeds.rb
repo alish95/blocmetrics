7545063ad45edf725ca752e934eb251b7972ebb5
Alice <alicewc95@gmail.com>
2018-05-17 14:48:02 -0700

    user sign in and out

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+User.create(email: 'test@user.com', password: 'password',
+  confirmed_at: Time.now
+)
+
+puts "Seed finished"
