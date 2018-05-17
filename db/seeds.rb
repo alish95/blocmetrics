0fe9e1e7d401a5bb7f49843437697f746fd8eac9
Alice <alicewc95@gmail.com>
2018-05-17 15:32:34 -0700

    associate events

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-User.destroy_all
+require 'faker'
 
 User.create(email: 'test@user.com', password: 'password',
   confirmed_at: Time.now
 )
 
+10.times do
+	RegisteredApplication.create!(
+		name: Faker::FamilyGuy.character,
+		url: Faker::Internet.url,
+		user: User.first
+		)
+end
+
+20.times do
+	Event.create!(
+		name: Faker::FamilyGuy.location,
+		registered_application: RegisteredApplication.first
+		)
+end
+
+
+
 puts "Seed finished"
+puts "#{RegisteredApplication.count} applications created"
+puts "#{Event.count} events created"
