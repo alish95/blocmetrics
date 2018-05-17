829652b5b64b9570fa12729db7011c9a13c3b55a
Alice <alicewc95@gmail.com>
2018-05-17 15:26:07 -0700

    register an application

---
+++ app/controllers/registered_applications_controller.rb
@@ -0,0 +1,40 @@
+class RegisteredApplicationsController < ApplicationController
+    def index
+       @registered_applications = current_user.registered_applications
+    end
+
+    def show
+      @app = RegisteredApplication.find(params[:id])
+    end
+
+    def new
+      @app = RegisteredApplication.new
+    end
+
+    def create
+      @app = current_user.registered_applications.new(app_params)
+      if @app.save
+        flash[:notice] = "Success!"
+        redirect_to registered_applications_path
+      else
+        flash.now[:alert] = "Please enter a valid name and URL."
+        render 'new'
+      end
+    end
+
+    def destroy
+      @app = RegisteredApplication.find(params[:id])
+      if @app.destroy
+        flash[:notice] = "Removed!"
+        redirect_to registered_applications_path
+      else
+        flash[:alert] = "Try again!"
+      end
+    end
+
+    private
+
+    def app_params
+      params.require(:registered_application).permit(:name, :url)
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'welcome/index'
+  resources :registered_applications, only: [:index, :new, :create, :show, :destroy]
 
+  devise_for :users
+  resources :users
+
+  get "welcome/index"
   get 'welcome/about'
 
-  root 'welcome#index'
+  root to: 'welcome#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
